dfed74f3abf20389d677605b4b7af251a6ee97a6
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-08 18:26:14 +0600

    problem two

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
+// function reverseString(str) {
+//   return str.split("").reverse().join("");
+// }
 
-// Example
-console.log(reverseString("hello"));
+// // Example
+// console.log(reverseString("hello"));
+
+// function countVowels(str) {
+//   let count = 0;
+//   const vowels = "aeiou";
+
+//   for (let char of str.toLowerCase()) {
+//     if (vowels.includes(char)) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// console.log(countVowels("Rashedul"));
