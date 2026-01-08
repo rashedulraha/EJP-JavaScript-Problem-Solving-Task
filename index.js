2e65d6b79ad52326fab40dc0c44a90266e0a67e3
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-08 18:53:56 +0600

    slove 4 number problem

--- index.js
+++ index.js
@@ -19,3 +19,10 @@
 // }
 
 // console.log(countVowels("Rashedul"));
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+// Example
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4])); // [1, 2, 3, 4]
